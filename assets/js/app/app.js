c757ad60e8e2c661479857c7d9c7169e7204e897
root <root@megatron.lindleys.org>
2015-04-02 18:18:21 -0500

    First Save. Nav bar created. Commit from megatron.

---
+++ assets/js/app/app.js
@@ -0,0 +1,33 @@
+var mrcApp = angular.module('mrc', ['ngRoute']);
+
+mrcApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/about-us', {
+				template: 'templates/about-us.html'
+			}).
+			when('/our-network', {
+				template: 'templates/our-network.html'
+			}).
+			when('/sales-and-pricing', {
+				template: 'templates/sales-and-pricing.html'
+			}).
+			when('/contact-us', {
+				template: 'templates/contact-us.html',
+				controller: 'ContactController'
+			}).
+			when('support', {
+				template: 'templates/support.html',
+				controller: 'SupportController'
+			}).
+			otherwise({
+				template: 'templates/main.html'
+			});
+	}
+]);
+
+mrcApp.controller('HeaderController', function($scope, $location) {
+	$scope.isActive = function(viewLocation) {
+		return viewLocation === $location.path;
+	};
+});
